03709bb98cb2fddb6ea7888d61a52cdc3cf1b5ff
Tim Torndal <tim.torndal@skola.botkyrka.se>
2020-10-18 21:51:51 +0200

    Solve 21

---
+++ A-Beginner/21/script.js
@@ -0,0 +1,9 @@
+//Skriv en funktion `isEven` som returnerar `true` om argumentet är jämnt och `false` om argumentet är udda
+
+function isEven(x) {
+    return x % 2 === 0 
+}
+
+console.log(isEven(1))
+
+console.log(isEven(8))
