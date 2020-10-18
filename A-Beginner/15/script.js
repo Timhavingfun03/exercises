2ae7cf6d20f958a1468033d5ff2cb239f8f3b8c2
Tim Torndal <tim.torndal@skola.botkyrka.se>
2020-10-18 20:03:16 +0200

    Solve 16

---
+++ A-Beginner/15/script.js
@@ -0,0 +1,15 @@
+//Skriv en funktion som multiplicerar två tal och returnerar resultatet
+
+const a = 3
+const b = 4
+
+function muliply(x, y){
+    return x * y
+}
+
+const multyplyer = muliply(a, b)
+
+console.log(a + '*' + b + '=' + multyplyer)
+
+
+
