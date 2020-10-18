c5a1a4335c945c0658994fabd7b0cc96245e3899
Tim Torndal <tim.torndal@skola.botkyrka.se>
2020-10-18 21:56:06 +0200

    Solve 22

---
+++ A-Beginner/20/script.js
@@ -0,0 +1,10 @@
+// Skriv en funktion som inte tar något argument och genererar ett random tal mellan 0 och 5
+
+function ARandomThing(){
+    return Math.random() * 5
+}
+
+console.log(ARandomThing())
+console.log(ARandomThing())
+console.log(ARandomThing())
+console.log(ARandomThing())
---
+++ A-Beginner/22/script.js
@@ -0,0 +1,10 @@
+//Skriv funktionen `isOdd`
+
+function isOdd(x){
+    return x % 2 === 1
+}
+
+console.log(isOdd(2))
+
+console.log(isOdd(1))
+
