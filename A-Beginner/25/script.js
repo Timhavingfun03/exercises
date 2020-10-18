8156b870ffd6c02cec941ebcb225ac86d69f47c3
Tim Torndal <tim.torndal@skola.botkyrka.se>
2020-10-18 22:16:54 +0200

    Solve 25

---
+++ A-Beginner/25/script.js
@@ -0,0 +1,11 @@
+//Skriv en funktion isLagom som returnerar true ifall argumentet är ett tal som är mellan 8 och 16 och annars returnerar false
+
+function isLagom(x){
+    return x > 8 && x < 16
+}
+
+console.log(isLagom(14))
+
+console.log(isLagom(5))
+
+
